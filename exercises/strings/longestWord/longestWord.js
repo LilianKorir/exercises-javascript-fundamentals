83b5f8c5c316ffaabf65007bc0cded9911e4582b
LilianKorir <59896177+LilianKorir@users.noreply.github.com>
2020-01-19 19:59:35 -0800

    longestWord

--- exercises/strings/longestWord/longestWord.js
+++ exercises/strings/longestWord/longestWord.js
@@ -8,12 +8,21 @@
  */
 
 function longestWord(string) {
+  let Array = string.split(' ')
+  counter = 0
+  for ( let i of Array) { //i=element
+if(i.length > counter){
+  counter=i.length
+  return i
+}
+  }
   // This is your job. :)
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for longestWord:');
-
+  console.log(longestWord('Running sanity checks for Word:'));
+  console.log(longestWord( 'what do you like'));
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
