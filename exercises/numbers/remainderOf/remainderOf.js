3796b25357e875bfa686dc240f750297ca4dd124
LilianKorir <59896177+LilianKorir@users.noreply.github.com>
2020-01-19 18:43:41 -0800

    Add files via upload

--- exercises/numbers/remainderOf/remainderOf.js
+++ exercises/numbers/remainderOf/remainderOf.js
@@ -15,17 +15,31 @@
  * @param {number} num The input number
  * @returns {boolean} True if num is even and false otherwise
  */
-function remainderOf(num) {
+function remainderOf(num1, num2) {
+  let quotient = num1/num2;
+  let remainder = num1-quotient*num2;
+
+
   /*
     This is your job. :)
 
     If you're not sure, step out of the code and use pen + paper. Start
     with simple examples and pay attention to the process you carry out.
   */
+ return remainder;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for remainderOf');
+  console.log(10 % 1); // 0
+  console.log(10 % 2); // 0
+  console.log(10 % 3); // 1
+  console.log(10 % 4); // 2
+  console.log(20 % 5); // 0
+  console.log(20 % 6); // 2
+  console.log(30 % 7); // 2
+  console.log(10 % 2.5); //0
+  console.log( 4.5 % 1.5) //0
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
