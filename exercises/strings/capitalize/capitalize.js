5ca8a098b2b66ed2afc287cbc718703d920bac79
LilianKorir <59896177+LilianKorir@users.noreply.github.com>
2020-01-19 19:51:27 -0800

    Capitalize

--- exercises/strings/capitalize/capitalize.js
+++ exercises/strings/capitalize/capitalize.js
@@ -10,12 +10,17 @@
  * @returns {string} A capitalized copy of the string
  */
 
-function capitalize(num) {
+function capitalize(string) {
+  firstLetter = string[0].toUpperCase()
+  otherLetters = string.slice(1)
+  return firstLetter + otherLetters
   // This is your job. :)
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for capitalize:');
+  console.log(capitalize('tureen')); //Turren
+  console.log(capitalize( 'tuesday')); // Tuesday
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
