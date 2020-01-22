7217a68e3a44a87ce0f073ff85909d4e8df06082
LilianKorir <59896177+LilianKorir@users.noreply.github.com>
2020-01-21 21:46:16 -0800

    fahrenheitToCelsius

--- exercises/numbers/fahrenheitToCelsius/fahrenheitToCelsius.js
+++ exercises/numbers/fahrenheitToCelsius/fahrenheitToCelsius.js
@@ -4,13 +4,17 @@
  * @param {number} temp - The temperature in Fahrenheit
  * @returns {boolean} The temperature in celsius
  */
-function fahrenheitToCelsius(temp) {
+function fahrenheitToCelsius(temp) {  
+  return (temp - 32)*5/9;
   // This is your job. :).
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for fahrenheitToCelsius:');
-
+console.log(fahrenheitToCelsius(32));
+console.log(fahrenheitToCelsius(198));
+console.log(fahrenheitToCelsius(57));
+console.log(fahrenheitToCelsius(0));
   // Your sanity checks go here.
 }
 
