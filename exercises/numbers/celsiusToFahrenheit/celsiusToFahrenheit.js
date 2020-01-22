31a990c7ac37f53a7ca0a68e4517578e723a9e07
LilianKorir <59896177+LilianKorir@users.noreply.github.com>
2020-01-21 21:39:55 -0800

    celsiusToFahrenheit

--- exercises/numbers/celsiusToFahrenheit/celsiusToFahrenheit.js
+++ exercises/numbers/celsiusToFahrenheit/celsiusToFahrenheit.js
@@ -5,13 +5,21 @@
  * @returns {boolean} The temperature in Fahrenheit
  */
 function celsiusToFahrenheit(temp) {
+
+  return (9/5)*temp + 32;
+  
+  };
+
+
   // This is your job. :).
-}
 
 if (require.main === module) {
   console.log('Running sanity checks for celsiusToFahrenheit:');
 
-  // Your sanity checks go here.
+  console.log(celsiusToFahrenheit(90));
+  console.log(celsiusToFahrenheit(0));// Your sanity checks go here.
 }
 
-module.exports = celsiusToFahrenheit;
+module.exports = celsiusToFahrenheit; 
+
+
