ab31053379c0a88b8fb6f5323fb0c3ded428f8c2
LilianKorir <59896177+LilianKorir@users.noreply.github.com>
2020-01-23 15:03:08 -0800

    selectAboveMean

--- exercises/arrays/selectAboveMean/selectAboveMean.js
+++ exercises/arrays/selectAboveMean/selectAboveMean.js
@@ -15,11 +15,25 @@
  *   greater than the mean.
  */
 function selectAboveMean(array) {
+  let total = 0;
+  for (element of array){
+    total += element;
+    mean = total/array.length}
+    let newArray = [];
+    for (element of array){
+      if (element > mean){
+        newArray.push(element);
+      }
+    }
+    return newArray;
   // This is your job. :)
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for selectAboveMean:');
+  console.log(selectAboveMean([1, 1, 1])); // => [])
+  console.log(selectAboveMean([1, 2, 3, 4, 5])); // => [4, 5])
+  console.log(selectAboveMean([ 10,20,30,40,50,60,70,80,90,100])); //[ 60, 70, 80, 90, 100 ]
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
