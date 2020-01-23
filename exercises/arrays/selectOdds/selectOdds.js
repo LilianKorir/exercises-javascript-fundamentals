2d28d177f9a370e198d5a30a2fe5156930efd0f0
LilianKorir <59896177+LilianKorir@users.noreply.github.com>
2020-01-23 15:21:29 -0800

    select.Odds

--- exercises/arrays/selectOdds/selectOdds.js
+++ exercises/arrays/selectOdds/selectOdds.js
@@ -14,11 +14,20 @@
  * @returns {number[]} A new array consisting of all the odd numbers in the input array
  */
 function selectOdds(array) {
+  let newArray = [];
+  for (element of array){
+    if (element % 2 !==0){
+      newArray.push(element);
+    }
+  }
+  return newArray;
   // This is your job. :)
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for selectOdds:');
+  console.log(selectOdds([1, 2, 3, 4, 5])); // => [1, 3, 5])
+  console.log(selectOdds([34,2,3,57,89,46,-7])); // [ 3, 57, 89, -7 ]
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
