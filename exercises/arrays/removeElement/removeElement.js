252a883333fff22c7e178bed0212628699088907
LilianKorir <59896177+LilianKorir@users.noreply.github.com>
2020-01-23 14:27:14 -0800

    removeElement

--- exercises/arrays/removeElement/removeElement.js
+++ exercises/arrays/removeElement/removeElement.js
@@ -14,11 +14,21 @@
  *   element removed.
  */
 function removeElement(array, element) {
+  let newArray = [];
+  for ( index = 0; index < array.length; index+=1){
+    if (array[index] !== element){
+      newArray.push(array[index]);
+    }
+  }
+ return newArray;
   // This is your job. :)
 }
 
 if (require.main === module) {
+  
   console.log('Running sanity checks for removeElement:');
+  console.log(removeElement([1,2,3,4,5,6,7,8,8,9,0,0,1], 0)); //[1,2,3,4,5,6,7,8,8,9,1]
+  console.log(removeElement(['lily','is', 'awesome','lovely'], 'awesome')); // ['lily', 'is', 'lovely']
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
