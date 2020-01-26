567758e3d96ec2e25d2746fdfb7cc16b9f921b73
LilianKorir <59896177+LilianKorir@users.noreply.github.com>
2020-01-26 10:39:57 -0800

    primeFactors

--- exercises/numbers/primeFactors/primeFactors.js
+++ exercises/numbers/primeFactors/primeFactors.js
@@ -13,7 +13,31 @@
  * @param {number} num - A positive integer
  * @returns {number[]} An array of all the prime factors of the given integer
  */
+function isPrime(num) {
+  if ( num === 1){
+     return false;}
+ 
+     if ( num === 2){return true;}
+     
+   for ( let n = 2; n < num; n++){
+   if (num % n === 0){
+    return false;
+   }
+   return true;
+ }
+}
+console.log(isPrime(3));
+
 function primeFactors(num) {
+  let array = [];
+  let i = 2; // i-quotient
+  for (i=2; i<= num; i++){
+if (num % i === 0){
+  if (isPrime(i) === true)
+    array.push(i);
+    }
+}
+  return array;
   /*
     Your code goes here.
 
@@ -27,7 +51,14 @@ function primeFactors(num) {
 
 if (require.main === module) {
   console.log('Running sanity checks for primeFactors:');
-
+  console.log(primeFactors(2)); // => [2]
+  console.log(primeFactors(2)); // => [2]
+  console.log(primeFactors(2 * 2)); // => [2, 2]
+  console.log(primeFactors(2 * 2 * 3)); // => [2, 2, 3]
+  console.log(primeFactors(2 * 2 * 3 * 17 * 197)); // => [2, 2, 3, 17, 197]
+  console.log(primeFactors(917329)); // => [7, 7, 97, 193]
+  
+  //
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
 }
