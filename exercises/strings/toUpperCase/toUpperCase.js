b467badf3e4a2a5ea52476a3a20a1403c37f86e8
Lilian Korir <likorir@davidson.edu>
2020-02-02 19:51:45 -0800

    toUpperCase

--- exercises/strings/toUpperCase/toUpperCase.js
+++ exercises/strings/toUpperCase/toUpperCase.js
@@ -16,11 +16,25 @@
  */
 
 function toUpperCase(string) {
+  let newstring = [];
+  for ( i = 0; i <= string.length; i++){
+    if ( string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122){
+     newstring.push(String.fromCharCode(string.charCodeAt(i)- 32));
+     
+    }
+else{
+  newstring.push(string[i]);
+  
+}
+}
+return newstring.join('');
   // This is your job. :)
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for toUpperCase:');
+  console.log(toUpperCase('whats up? '));
+  console.log(toUpperCase('this is a new decade!'))
 
   // Add your own sanity checks here.
   // How else will you be sure your code does what you think it does?
